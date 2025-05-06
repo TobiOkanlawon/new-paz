5b6a6c3160b7f3359e76900420633fe0e097e515
LeviUdoh-17 <leviudoh17@gmail.com>
2025-05-06 20:33:45 +0100

    Added login UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,40 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import React from "react";
+import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <Image src={'/PAZLogo.png'} alt="Logo" width={127} height={42.61} className={styles.logo} />
+          <h1>Welcome</h1>
+          <p>Powering Your Dreams!</p>
+          <div className={styles.spacer}></div>
+          <Image src={'/PazIlls2.png'} alt="PazIlls" width={736.59} height={488} className={styles.PazIlls}/>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className={styles.right}>
+          <h3>Login to your account</h3>
+          <form action="GET" className={styles.form}>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" placeholder="Enter your email address" name="email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" placeholder="Enter your password" id="password" />
+            <div className={styles.formBottom}>
+              <div className={styles.rememberMe}>
+                <input type="checkbox" className={styles.remember} name="remember" id="remember" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              <div>
+                <a href="#">Forgot Password?</a>
+              </div>
+            </div>
+            <button className={styles.login} type="submit">Login</button>
+            <a href="#" className={styles.createNewAccount}>Create New Account <FaArrowRight/> </a>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
