52fbfc15551caaccee9a4756c4a416f1624be472
Tobi Okanlawon <tobiinlondon34@gmail.com>
2025-05-07 17:17:34 +0100

    Create input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./input.module.css";
+import clsx from "clsx";
+
+type Props = {
+  label: string;
+} & React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+const Input = ({ label, id, className, ...rest }: Props) => {
+  return (
+    <div className="form-group">
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      <input id={id} className={clsx(styles.input, className)} {...rest} />
+    </div>
+  );
+};
+
+export default Input;
