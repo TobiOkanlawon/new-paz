301599b2496f3c07e5da76f638cab85707d7eafd
LeviUdoh-17 <leviudoh17@gmail.com>
2026-02-20 09:44:41 +0100

    Created the onboarding page and it is responsive

--- src/components/onboarding/LeftCaurosel.tsx
+++ src/components/onboarding/LeftCaurosel.tsx
@@ -1,16 +1,17 @@
 "use client";
 import { useState, useCallback, useEffect } from "react";
 import Image from "next/image";
+import { FaArrowRight, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import styles from './LeftStyle.module.css';
-import LeftText from "./LeftText";
+import Button from "../Button";
 
-const SLIDE_DURATION = 6000; // 5 seconds per slide
+const SLIDE_DURATION = 6000;
 
 const LeftCaurosel = () => {
     const bgImages = [
-        { bg: "/images/leftBG1.png", text: "PAZ is a simple and secure platform that helps people save smarter and manage payments with ease. It is built for everyday users who want clarity, control, and peace of mind when handling their money." },
-        { bg: "/images/leftBG2.png", text: "With PAZ, users can create an account in minutes, verify their identity seamlessly and start saving and making withdrawal without unnecessary complexity. Every feature is designed to feel intuitive, fast and trustworthy." },
-        { bg: "/images/leftBG3.png", text: "PAZ is a simple and secure platform that helps people save smarter and manage payments with ease. It is built for everyday users who want clarity, control, and peace of mind when handling their money." },
+        { bg: "/images/leftBG11.png", text: "PAZ is a simple and secure platform that helps people save smarter and manage payments with ease. It is built for everyday users who want clarity, control, and peace of mind when handling their money." },
+        { bg: "/images/leftBG21.png", text: "With PAZ, users can create an account in minutes, verify their identity seamlessly and start saving and making withdrawal without unnecessary complexity. Every feature is designed to feel intuitive, fast and trustworthy." },
+        { bg: "/images/leftBG31.png", text: "PAZ is a simple and secure platform that helps people save smarter and manage payments with ease. It is built for everyday users who want clarity, control, and peace of mind when handling their money." },
     ]
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -52,29 +53,73 @@ const LeftCaurosel = () => {
         }, 300);
     }, [bgImages.length]);
 
+    const skip = useCallback(() => {
+        setCurrentImageIndex(bgImages.length - 1);
+        setIsAutoPlaying(false);
+    }, [bgImages.length]);
+
     return (
         <div className={styles.container}>
-            <Image
-                src={bgImages[currentImageIndex].bg}
-                alt="Background Image"
-                fill
-                style={{ objectFit: "cover" }}
-                className={`${styles.bgImages} ${styles.fadeIn}`}
-            />
+            <div className={styles.bgContainer}>
+                <div className={styles.logoContainer}>
+                    <Image src="/images/pazLeftBG.png" alt="PAZ Logo" width={158} height={51} className={styles.logo} />
+                    <div className={styles.onboardingSkip} onClick={skip}>
+                        <p>Skip</p>
+                    </div>
+                </div>
 
-            <div className={styles.overlay}></div>
-            <Image src="/images/pazLeftBG.png" alt="PAZ Logo" width={700} height={237.01} className={styles.logo} />
-            <div className={styles.content}>
-                <LeftText
-                    text={bgImages[currentImageIndex].text}
-                    currentIndex={currentImageIndex}
-                    totalSlides={bgImages.length}
-                    onNext={goToNext}
-                    onPrev={goToPrev}
-                    animationKey={animationKey}
-                    slideDuration={SLIDE_DURATION}
-                    isAutoPlaying={isAutoPlaying}
-                />
+                <div className={styles.onboardingImage}>
+                    <Image
+                        src={bgImages[currentImageIndex].bg}
+                        alt="Background Image"
+                        width={611}
+                        height={358}
+                        style={{ objectFit: "cover" }}
+                        className={`${styles.bgImages} ${styles.fadeIn}`}
+                    />
+                </div>
+
+                <div className={styles.textContainer}>
+                    <p className={styles.text}>{bgImages[currentImageIndex].text}</p>
+                </div>
+
+                <div className={styles.indicatorContainer}>
+                    <FaChevronLeft 
+                        className={styles.chevronIcon} 
+                        onClick={goToPrev} 
+                        style={{ cursor: 'pointer' }} 
+                    />
+                    <div className={styles.indicatorsContainer}>
+                        {bgImages.map((_, index) => (
+                            <span 
+                                key={index} 
+                                className={`${styles.indicator} ${currentImageIndex === index ? styles.indicatorActive : ''}`}
+                            >
+                                {currentImageIndex === index && isAutoPlaying && (
+                                    <span 
+                                        key={animationKey} 
+                                        className={styles.loader}
+                                        style={{ animationDuration: `${SLIDE_DURATION}ms` }}
+                                    />
+                                )}
+                            </span>
+                        ))}
+                    </div>
+                    <FaChevronRight 
+                        className={styles.chevronIcon} 
+                        onClick={goToNext} 
+                        style={{ cursor: 'pointer' }} 
+                    />
+                </div>
+
+                <div className={styles.buttonContainer}>
+                    <div className={styles.loginButton}>
+                        <Button variant="outlined">Login</Button>
+                    </div>
+                    <button className={styles.nextButton} onClick={goToNext}>
+                        Next <FaArrowRight />
+                    </button>
+                </div>
             </div>
         </div>
     );
