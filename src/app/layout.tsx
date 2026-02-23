fd9e7ad08f8f63fbf008ea16eae46e6f77298c4f
Tobi Okanlawon <tobiinlondon34@gmail.com>
2026-02-23 13:29:47 +0100

    lint and use proper typing for page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Poppins, Montserrat, Inter } from "next/font/google";
 import Providers from "@/components/globals/providers";
+import { Metadata } from "next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -20,12 +21,12 @@ const inter = Inter({
   weight: ["400", "500", "600"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "PAZ",
   description: "Powering Dreams",
   icons: {
-    icon: '/favicon.png',
-    shortcut: '/favicon.png',
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
   },
 };
 
@@ -41,8 +42,15 @@ export default function RootLayout({
     >
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <Providers>{children}</Providers>
