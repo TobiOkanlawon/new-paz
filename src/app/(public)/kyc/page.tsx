0105c8e89cf3e4f62452383353751d4567a80a17
Tobi Okanlawon <tobiinlondon34@gmail.com>
2026-03-04 12:46:54 +0100

    Fix problem where the page is named and treated like a layout

--- src/app/(public)/kyc/page.tsx
+++ src/app/(public)/kyc/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
 import styles from "./kyc.module.css";
 import KYCVerification from "@/components/KYCVerification/kycverifications";
 
 import LeftCaurosel from "@/components/onboarding/LeftCaurosel";
 
-export default function OnboardingLayout({
-  children,
-}: {
-    children: React.ReactNode;
-}) {
+export default function () {
   return (
     <div className={styles.container}>
       <div className={styles.leftContainer}>
