8849c68a4b0cb029487fedfff665cbe6d932d384
Tobi Okanlawon <tobiinlondon34@gmail.com>
2026-04-14 09:43:32 +0100

    do some work on phone verification page, redirect to create page
    I implement a temporary redirect to the savings create page when the
    user has not created a solo savings account yet.

--- src/app/(private)/dashboard/savings/solo-saver/page.tsx
+++ src/app/(private)/dashboard/savings/solo-saver/page.tsx
@@ -5,6 +5,7 @@ import {
   getSavingsTransactions,
 } from "@/actions/transactions";
 import SoloSaver from "./SoloSaver";
+import { redirect } from "next/navigation";
 import { getAccountSummary } from "@/actions/dashboard";
 
 export default async function Page() {
@@ -14,19 +15,23 @@ export default async function Page() {
     throw new Error("failed to load account");
   }
 
-  const transactions = await getSavingsTransactions(
-    accountDetails.data.soloSavings.accountNo,
-  );
+  if (accountDetails.data.hasSoloAccount) {
+    const transactions = await getSavingsTransactions(
+      accountDetails.data.soloSavings.accountNo,
+    );
 
-  if (!transactions.success) {
-    // throw new Error("failed to get transactions");
-    return <SoloSaver transactions={[]} accountDetails={accountDetails.data} />;
+    if (!transactions.success) {
+      return (
+        <SoloSaver transactions={[]} accountDetails={accountDetails.data} />
+      );
+    }
+    return (
+      <SoloSaver
+        transactions={transactions.data}
+        accountDetails={accountDetails.data}
+      />
+    );
+  } else {
+    redirect("/dashboard/savings/create");
   }
-
-  return (
-    <SoloSaver
-      transactions={transactions.data}
-      accountDetails={accountDetails.data}
-    />
-  );
 }
